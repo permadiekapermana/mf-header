94524a249c9caf9e5401f61e01bfa29fcc1ef3ba
jumadi <aa.jumadi@gmail.com>
2022-10-07 16:06:07 +0700

    Breadcrumb

--- src/components/AppBreadcrumb.vue
+++ src/components/AppBreadcrumb.vue
@@ -1,46 +1,32 @@
 <template>
-  <CBreadcrumb class="d-md-down-none me-auto mb-0">
+  <CBreadcrumb class="d-md-down-none me-auto mb-0" id="AppBreadcrumb">
     <CBreadcrumbItem
-      v-for="item in breadcrumbs"
+      v-for="item in breadcrumbsJsonValue"
       :key="item"
       :href="item.active ? '' : item.path"
       :active="item.active"
     >
       {{ item.name }}
     </CBreadcrumbItem>
-  </CBreadcrumb>
+    <CButton @click="breadcrumbsStrJsonValueChange()" id="breadcrumbsChangeValue" 
+    :style="{visibility: 'hidden'}">abc</CButton>
+  </CBreadcrumb>  
 </template>
 
 <script>
-import { onMounted, ref } from 'vue'
-import router from '@/router'
 
 export default {
+  el:'#AppBreadcrumb',
   name: 'AppBreadcrumb',
-  setup() {
-    const breadcrumbs = ref()
-
-    const getBreadcrumbs = () => {
-      return router.currentRoute.value.matched.map((route) => {
-        return {
-          active: route.path === router.currentRoute.value.fullPath,
-          name: route.name,
-          path: `${router.options.history.base}${route.path}`,
-        }
-      })
-    }
-
-    router.afterEach(() => {
-      breadcrumbs.value = getBreadcrumbs()
-    })
-
-    onMounted(() => {
-      breadcrumbs.value = getBreadcrumbs()
-    })
-
-    return {
-      breadcrumbs,
-    }
+  data() {
+      return {
+        breadcrumbsJsonValue: [{"active":false,"name":"Home","path":"/#/"}]
+      }
   },
+  methods: {
+    breadcrumbsStrJsonValueChange(){
+      this.breadcrumbsJsonValue = JSON.parse(localStorage.breadcrumbs)
+    }
+  }
 }
 </script>
