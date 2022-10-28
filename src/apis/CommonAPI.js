296b0d9f75b891747538c4b89a86b7d90be2b298
jumadi <aa.jumadi@gmail.com>
2022-10-28 10:44:43 +0700

    Breadcrumbs

--- src/apis/CommonAPI.js
+++ src/apis/CommonAPI.js
@@ -1,51 +1,68 @@
-
-import utils from "@/utils/CommonUtils"
+import Swal from 'sweetalert2'
+import router from '../router'
 
 export default {
+    error(message, callback) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: message || 'Something went wrong!',
+          }).then(() => {
+            if(callback) callback();
+          });
+    },
+    backToPreviousPage() {
+        Swal.fire({
+          title: 'Back to previous page?',
+          text: 'All filed forms will be lost!',
+          showCancelButton: true,
+          confirmButtonText: 'Yes',
+          cancelButtonText: `No`,
+          customClass: {
+            confirmButton: 'btn btn-primary me-4',
+            cancelButton: 'btn btn-outline-primary',
+          },
+          buttonsStyling: false,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            router.back()
+          }
+        })
+    },
     async jsonApi( uri, method, jsonBody){
-      try{
-        let response = await fetch( process.env.VUE_APP_SC_API_URL + uri, {
-            method: method, // *GET, POST, PUT, DELETE, etc.
-            headers: {
-              'Content-Type': 'application/json',
-              "Authorization": 'Bearer ' + localStorage.id_token,
-            },
-            body: jsonBody
-        } );
-        let jsonResponse = await response.json();
-        if(jsonResponse.status==='401'){
-          // utils.error(jsonResponse.detail,()=>{
-            localStorage.id_token = '';
-            window.location.href=process.env.VUE_APP_CONTAINER_URL;            
-          // });
-        }
-        return jsonResponse;
-      }catch(e){       
-        // console.log(e.message+('Failed to fetch'==e.message?' BE API':''))
-        utils.error(e.message+('Failed to fetch'==e.message?' BE API':''));
+      let response = await fetch( process.env.VUE_APP_SC_API_URL + uri, {
+          method: method, // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            'Content-Type': 'application/json',
+            "Authorization": 'Bearer ' + localStorage.id_token,
+          },
+          body: jsonBody
+      } );
+      let jsonResponse = await response.json();
+      if(jsonResponse.status==='401'){
+        // this.error(jsonResponse.detail,()=>{
+          localStorage.id_token = '';
+          window.location.href=process.env.VUE_APP_CONTAINER_URL;            
+        // });
       }
+      return jsonResponse;
     },
     async queryApi( uri, method, queryParam){
-      try{
-        let response = await fetch( process.env.VUE_APP_SC_API_URL + uri + (this.isEmpty(queryParam)?'':('?' + queryParam)), {
-            method: method, // *GET, POST, PUT, DELETE, etc.
-            headers: {
-              'Content-Type': 'application/json',
-              "Authorization": 'Bearer ' + localStorage.id_token,
-            },
-        } );
-        let jsonResponse = await response.json();
-        if(jsonResponse.status==='401'){
-          // utils.error(jsonResponse.detail,()=>{
-            localStorage.id_token = '';
-            window.location.href=process.env.VUE_APP_CONTAINER_URL;            
-          // });
-        }
-        return jsonResponse;
-      }catch(e){        
-        // console.log(e.message+('Failed to fetch'==e.message?' BE API':''))
-        utils.error(e.message+('Failed to fetch'==e.message?' BE API':''));
+      let response = await fetch( process.env.VUE_APP_SC_API_URL + uri + (this.isEmpty(queryParam)?'':('?' + queryParam)), {
+          method: method, // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            'Content-Type': 'application/json',
+            "Authorization": 'Bearer ' + localStorage.id_token,
+          },
+      } );
+      let jsonResponse = await response.json();
+      if(jsonResponse.status==='401'){
+        // this.error(jsonResponse.detail,()=>{
+          localStorage.id_token = '';
+          window.location.href=process.env.VUE_APP_CONTAINER_URL;            
+        // });
       }
+      return jsonResponse;
     },
     isEmpty(obj){
       return obj==null || obj===undefined || obj==='';
--- src/components/AppBreadcrumb.vue
+++ src/components/AppBreadcrumb.vue
@@ -4,12 +4,13 @@
       v-for="item in breadcrumbsJsonValue"
       :key="item"
       :href="item.active ? '' : item.path"
-      :active="item.active"
+      :active="item.active"      
+      v-on:click="onClickBreadcrumb"
     >
       {{ item.name }}
     </CBreadcrumbItem>
     <CButton @click="breadcrumbsStrJsonValueChange()" id="breadcrumbsChangeValue" 
-    :style="{visibility: 'hidden'}">abc</CButton>
+    :style="{visibility: 'hidden'}">BreadcrumbsHelper</CButton>
   </CBreadcrumb>  
 </template>
 
@@ -20,12 +21,26 @@ export default {
   name: 'AppBreadcrumb',
   data() {
       return {
-        breadcrumbsJsonValue: [{"active":false,"name":"Home","path":"/#/"}]
+        breadcrumbsJsonValue: [{"active":false,"name":"Home","path":"#/sc/dashboard"}]
       }
   },
   methods: {
     breadcrumbsStrJsonValueChange(){
+      // console.log('mf-header '+localStorage.breadcrumbs);
+      // console.log('mf-header '+JSON.parse(localStorage.breadcrumbs))
       this.breadcrumbsJsonValue = JSON.parse(localStorage.breadcrumbs)
+    },
+    onClickBreadcrumb(e){
+      let nextBreadCrumbLiElement = e.target.parentNode.nextElementSibling;
+      while(nextBreadCrumbLiElement){
+        console.log(nextBreadCrumbLiElement.tagName)
+        if(nextBreadCrumbLiElement.tagName==='LI'){
+          nextBreadCrumbLiElement.remove();
+          nextBreadCrumbLiElement = e.target.parentNode.nextElementSibling;
+        }else{
+          nextBreadCrumbLiElement=undefined;
+        }
+      }
     }
   }
 }
